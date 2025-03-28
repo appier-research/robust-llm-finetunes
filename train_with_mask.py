8f8866cf50089cfea15670ca22dec83a2e356baa
Johnson Wu <johnson.wu@appier.com>
2025-03-28 15:45:23 +0800

    oom issue

--- train_with_mask.py
+++ train_with_mask.py
@@ -16,6 +16,8 @@ import torch
 from accelerate import PartialState
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import deepspeed
+from liger_kernel.transformers import apply_liger_kernel_to_llama
+apply_liger_kernel_to_llama()
 # Add argument parser
 parser = argparse.ArgumentParser(description='Train model with custom learning rate and threshold')
 parser.add_argument('--learning_rate', type=float, default=2e-5,
@@ -35,10 +37,10 @@ args_cli = parser.parse_args()
 
 
 # Set device if using deepspeed
-device = torch.device("cuda", args_cli.local_rank) if torch.cuda.is_available() else torch.device("cpu")
+# device = torch.device("cuda", args_cli.local_rank) if torch.cuda.is_available() else torch.device("cpu")
 
 # Initialize DeepSpeed
-deepspeed.init_distributed()
+# deepspeed.init_distributed()
 
 
 # model_name = 'gemma'
@@ -60,31 +62,38 @@ tokenizer = AutoTokenizer.from_pretrained(target_model)
 tokenizer.pad_token = tokenizer.eos_token
 target_model = AutoModelForCausalLM.from_pretrained(
     args_cli.target_model,
-    # torch_dtype=torch.bfloat16,
+    torch_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2",
     device_map={'':device_string}
 )
+for param in target_model.get_input_embeddings().parameters():
+    param.requires_grad = False
+for param in target_model.get_output_embeddings().parameters():
+    param.requires_grad = False
 
-peft_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    lora_dropout=0.05,
-    target_modules="all-linear",
-    task_type="CAUSAL_LM",
-)
-args = SFTConfig(output_dir="trained/models"+f"{model_name}_{dataset_name}_config_lr{args_cli.learning_rate}_t{args_cli.threshold}_sft",
+    
+# peft_config = LoraConfig(
+#     r=16,
+#     lora_alpha=32,
+#     lora_dropout=0.05,
+#     target_modules="all-linear",
+#     task_type="CAUSAL_LM",
+# )
+args = SFTConfig(output_dir="/workspace/sharing/johnsonwu/trained_models/"+f"{model_name}_{dataset_name}_config_lr{args_cli.learning_rate}_t{args_cli.threshold}_sft",
     gradient_checkpointing=True,
-    num_train_epochs = 6,
+    num_train_epochs = 1,
     use_liger=False,
     learning_rate=args_cli.learning_rate,  # Use command line argument
     save_strategy="epoch",
     save_steps=1,
     per_device_train_batch_size=1,
-    gradient_accumulation_steps=1,
+    gradient_accumulation_steps=10,
     warmup_steps=20,
     logging_steps=1,
     packing=False,
     gradient_checkpointing_kwargs={"use_reentrant": False},
-    deepspeed="deepspeed_configs/zero3_bf16_cpuoffload_all.json"
+    deepspeed="deepspeed_configs/zero3.json",
+    optim='adamw_8bit',
 )
 args.skip_prepare_dataset = True
 args.remove_unused_columns = False
@@ -93,6 +102,6 @@ trainer = SFTTrainer(
     train_dataset=dataset,
     args=args,
     # peft_config=peft_config,
-    data_collator=CustomizedDataCollatorForChatML(tokenizer, threshold=args_cli.threshold, apply="highest")  # Use command line argument
+    data_collator=CustomizedDataCollatorForChatML(tokenizer, threshold=args_cli.threshold, apply="None")  # Use command line argument
 )
-trainer.train("trained_models/checkpoint-282")
+trainer.train()
