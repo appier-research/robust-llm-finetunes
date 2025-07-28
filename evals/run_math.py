5ea67bd38c66799f516549ce6ac7de778dbf35b9
ray-tam-appier <164005491+ray-tam-appier@users.noreply.github.com>
2025-07-28 17:41:01 +0800

    Update run_math.py

--- evals/run_math.py
+++ evals/run_math.py
@@ -12,7 +12,8 @@ from transformers import pipeline
 from .common import check_equality, ANSWER_PATTERN
 from .openai_sampler import ChatCompletionSampler
 from .normalization import math_normalizer
-# from together import Together
+from llms.sglang_inference import SGLangInference
+
 
 QUERY_TEMPLATE = """
 Solve the following math problem step by step. The last line of your response should be of the form Answer: $ANSWER (without quotes) where $ANSWER is the answer to the problem.
@@ -34,7 +35,7 @@ def validate_checkpoint(load_adapter,
         torch_dtype=None
     ):
     import glob
-    dataset = load_dataset("arrow", data_files={"validation":"dataset/ground_truth/math/validation/data-00000-of-00001.arrow"},split="validation")
+    dataset = load_dataset("appier-ai-research/robust-finetuning", "math-resample")['validation']
     checkpoint2scores = {}
     mini_sampler = ChatCompletionSampler(model="gpt-4o-mini-2024-07-18")
     for checkpoint in glob.glob(load_adapter):
@@ -42,7 +43,7 @@ def validate_checkpoint(load_adapter,
             pipe = pipeline("text-generation", base_model,
                             device_map='cuda', torch_dtype=torch.bfloat16)
         else:
-            pipe = pipeline("text-generation", base_model, device_map='cuda')
+            pipe = pipeline("text-generation", base_model)
         print(checkpoint)
         pipe.model.load_adapter(checkpoint)
         total = 0
@@ -61,113 +62,46 @@ def validate_checkpoint(load_adapter,
             correct = float(check_equality(mini_sampler, gold, extracted_answer))
             correct_cnt += correct
             total += 1
-            # if total == 200:
-            #     break
+            if total == 200:
+                break
         checkpoint2scores[checkpoint] = correct_cnt / total
         print(checkpoint, correct_cnt / total)
         del pipe
     print(base_model)
     for ckpt, pass_score in checkpoint2scores.items():
         print(ckpt, pass_score)
-    with open('logging/math-val/validation.jsonl', "a") as fout:
-            fout.write(json.dumps(checkpoint2scores)+'\n')
-
-def use_custom_gpt4(messages, model_name):
-    client = OpenAI(api_key=os.environ['OPENAI_API_KEY'] , organization=os.environ['OPENAI_ORGANIZATION'])
-    # client = Together(api_key=os.environ["TOGETHER_API_KEY"])
-    response = client.chat.completions.create(
-      model=model_name,
-      messages=messages,
-      temperature=1,
-      max_tokens=512,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0
-    ).choices[0].message.content
-    return response
-
-
-def use_nim_api(message):
-    client = OpenAI(
-      base_url = "https://integrate.api.nvidia.com/v1",
-      api_key = "api-key"
-    )
-    
-    completion = client.chat.completions.create(
-      model="qwen/qwen2-7b-instruct",
-      messages=message,
-      temperature=0,
-      top_p=0.95,
-      max_tokens=4096
-    ).choices[0].message.content
-    return completion
-
 
 def main(load_adapter, few_shot=0, resume=None, base_model="unsloth/llama-3-8b-Instruct-bnb-4bit", 
-            torch_dtype=None, run_val=False, sanity_check=False):
-    if run_val and load_adapter and '*' in load_adapter:
+            torch_dtype='bf16', run_val=False, use_sglang=False):
+    if run_val and '*' in load_adapter:
         validate_checkpoint(load_adapter, base_model=base_model, torch_dtype=torch_dtype)
         return 2
-    if run_val:
-        log_dir = Path("logging")
-        log_dir.mkdir(exist_ok=True)
-        if few_shot == 0:
-            log_dir = Path("logging/math-val")
-            log_dir.mkdir(exist_ok=True)
-        else:
-            log_dir = Path("logging/math-val-{}".format(few_shot))
-            log_dir.mkdir(exist_ok=True)
-    else:
-        log_dir = Path("logging")
-        log_dir.mkdir(exist_ok=True)
-        if few_shot == 0:
-            log_dir = Path("logging/math-test")
-            log_dir.mkdir(exist_ok=True)
-        else:
-            log_dir = Path("logging/math-test-{}".format(few_shot))
-            log_dir.mkdir(exist_ok=True)
 
-    dataset = load_dataset("arrow", data_files={"train":"dataset/ground_truth/math/train/data-00000-of-00001.arrow","validation":"dataset/ground_truth/math/validation/data-00000-of-00001.arrow", "test":"dataset/ground_truth/math/test/data-00000-of-00001.arrow"})
-    if sanity_check:
-        total = 0
-        oai_correct_cnt = 0
-        correct_cnt = 0
-        for split in ['train','validation','test']:
-            mini_sampler = ChatCompletionSampler(model="gpt-4o-mini-2024-07-18")
-            dataset_ = dataset[split]
-            print(split)
-            with tqdm(total=len(dataset_), dynamic_ncols=True) as pbar:
-                for idx, row in enumerate(dataset_):
-                    match = re.search(ANSWER_PATTERN, row['solution'])
-                    extracted_answer = match.group(1) if match else math_normalizer(row['solution'])
-                    gold = math_normalizer(row['solution'])
-                    correct = float(check_equality(mini_sampler, gold, extracted_answer))
-                    if correct:
-                        correct_cnt += 1
-                    total += 1
-    
-                    pbar.update(1)
-                    pbar.set_description("[pass={:.2f}]".format(100*correct_cnt/total))
-        return None
-    if 'gpt-' in base_model:
+    log_dir = Path("logging")
+    log_dir.mkdir(exist_ok=True)
+    if few_shot == 0:
+        log_dir = Path("logging/math-test")
+        log_dir.mkdir(exist_ok=True)
+    else:
+        log_dir = Path("logging/math-test-{}".format(few_shot))
+        log_dir.mkdir(exist_ok=True)
+
+    dataset = load_dataset("appier-ai-research/robust-finetuning", "math-resample")
+
+    if use_sglang:
+        pipe = SGLangInference(base_model, load_adapter)
+        load_adapter = load_adapter or base_model
+    elif 'gpt' in base_model:
         load_adapter = "openai_"+base_model
-        pipe=None
-    elif "nim-" in base_model:
-        load_adapter = base_model
-        pipe=None
     else:
         if torch_dtype=='bf16':
-            pipe = pipeline("text-generation", base_model, device_map='auto', torch_dtype=torch.bfloat16)
+            pipe = pipeline("text-generation", base_model, device_map='cuda', torch_dtype=torch.bfloat16)
         else:
             pipe = pipeline("text-generation", base_model)
         if load_adapter:
             pipe.model.load_adapter(load_adapter)
         else:
             load_adapter = base_model
-    # if 'mistral' in base_model:
-    #     from transformers import AutoTokenizer
-    #     tokenizer = AutoTokenizer.from_pretrained(base_model)
-    #     pipe.model.generation_config.pad_token_id = tokenizer.unk_token_id
     total = 0
     correct_cnt = 0
     fmt_fail_cnt = 0
@@ -198,28 +132,27 @@ def main(load_adapter, few_shot=0, resume=None, base_model="unsloth/llama-3-8b-I
                 break
             few_shot_text += '\nProblem: {}\nAnswer: {}'.format(row['problem'], row['solution'])
     few_shot_text = few_shot_text.strip()
-    if run_val:
-        split = 'validation'
-    else:
-        split = 'test'
-    with tqdm(total=len(dataset[split]), dynamic_ncols=True) as pbar:
-        for idx, row in enumerate(dataset[split]):
+
+    with tqdm(total=len(dataset['test']), dynamic_ncols=True) as pbar:
+        for idx, row in enumerate(dataset['test']):
             if idx in added:
                 pbar.update(1)
                 continue
 
             problem = row['problem']
             test_question = QUERY_TEMPLATE.format(Question=problem)
-            messages = [
-                {"role": "user", "content": test_question},
-            ]
-            if "gpt" in base_model or "nim" in base_model:
-                if "gpt-" in base_model:
-                    generation = use_custom_gpt4(messages, base_model.replace("gpt-", ""))
-                else:
-                    generation = use_nim_api(messages)
-                    
+            # test_question = "\nProblem: " + problem + "\n" + "Answer: $"
+            # input_text = train_prompt
+            # if few_shot > 0:
+            #     input_text += '\nExamples:\n'+few_shot_text+'\n'
+            #     input_text += 'Now answer this question:\n'
+            # input_text += test_question
+            if use_sglang:
+                generation, _ = pipe(test_question, max_tokens=512, temperature=0.0)
             else:
+                messages = [
+                    {"role": "user", "content": test_question},
+                ]
                 generation = pipe(messages, max_new_tokens=512)[0]['generated_text'][-1]['content']
             match = re.search(ANSWER_PATTERN, generation)
             extracted_answer = match.group(1) if match else math_normalizer(generation)
@@ -255,11 +188,11 @@ if __name__ == "__main__":
     parser.add_argument("--shots", type=int, help="Path to the adapter file to load", default=0)
     parser.add_argument("--resume", type=str, help="Path to the adapter file to load", default=None)
     parser.add_argument("--torch_dtype", type=str, help="torch dtype for model", default=None)
-    parser.add_argument("--env_sanity_check",  dest='env_sanity_check', action='store_true', help="check validation of testing set", default=False)
     parser.add_argument("--run_val",  dest='run_val', action='store_true', help="run validation split", default=False)
+    parser.add_argument("--use_sglang", action="store_true", help="Use SGLang inference engine")
     args = parser.parse_args()
     main(args.load_adapter, args.shots, args.resume, args.base_model,
         torch_dtype=args.torch_dtype,
         run_val=args.run_val,
-        sanity_check=args.env_sanity_check
+        use_sglang=args.use_sglang
     )
